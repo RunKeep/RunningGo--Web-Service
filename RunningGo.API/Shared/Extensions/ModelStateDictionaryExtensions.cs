73c7ad3e8e656ab761424fa7e7185d5ce77a0a2f
mauriprado <104120014+mauriprado@users.noreply.github.com>
2022-06-05 22:13:11 -0500

    feat: extension methods
    Added extension methods and base response.

---
+++ RunningGo.API/Shared/Extensions/ModelStateDictionaryExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace RunningGo.API.Shared.Extensions;
+
+public static class ModelStateDictionaryExtensions
+{
+    public static List<string> GetErrorMessages(this ModelStateDictionary dictionary)
+    {
+        return dictionary.SelectMany(m => m.Value.Errors)
+            .Select(m => m.ErrorMessage)
+            .ToList();
+    }
+}
