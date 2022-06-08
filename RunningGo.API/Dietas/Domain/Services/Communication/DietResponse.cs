9878ec55b79851a7159b52ea3dcc21f3925d75f2
mauriprado <104120014+mauriprado@users.noreply.github.com>
2022-06-08 00:45:25 -0500

    feat(dietas): Added and implemented Diet repository.

---
+++ RunningGo.API/Dietas/Domain/Services/Communication/DietResponse.cs
@@ -0,0 +1,10 @@
+﻿using RunningGo.API.Dietas.Domain.Models;
+using RunningGo.API.Shared.Domain.Services.Communication;
+
+namespace RunningGo.API.Dietas.Domain.Services.Communication;
+
+public class DietResponse: BaseResponse<Diet>
+{
+    public DietResponse(string message): base(message) { }
+    public DietResponse(Diet resource): base(resource) { }
+}
