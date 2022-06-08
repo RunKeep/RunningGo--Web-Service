02fb42cdef9402b35ccefabfb2970ed17ca0e4f3
mauriprado <104120014+mauriprado@users.noreply.github.com>
2022-06-07 23:42:59 -0500

    feat(dietas): Added Diet model and resources.

---
+++ RunningGo.API/Dietas/Resources/SaveDietResource.cs
@@ -0,0 +1,12 @@
+﻿namespace RunningGo.API.Dietas.Resources;
+
+public class SaveDietResource
+{
+    public string Description { set; get; }
+    public string Specs { set; get; }
+    public string Duration { set; get; }
+    
+    //Relationships
+    public int FoodId { set; get; }
+    public int UserId { set; get; }
+}
