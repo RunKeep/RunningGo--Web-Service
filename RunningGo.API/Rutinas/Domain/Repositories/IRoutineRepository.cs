b90fbd3e20f6e9064f3bd0d0ba317e663657275c
mauriprado <104120014+mauriprado@users.noreply.github.com>
2022-06-13 16:59:21 -0500

    feat(routines): Added Routine Repository.

---
+++ RunningGo.API/Rutinas/Domain/Repositories/IRoutineRepository.cs
@@ -0,0 +1,13 @@
+﻿using RunningGo.API.Rutinas.Domain.Models;
+
+namespace RunningGo.API.Rutinas.Domain.Repositories;
+
+public interface IRoutineRepository
+{
+    Task<IEnumerable<Routine>> List();
+    Task<IEnumerable<Routine>> ListByUserId(long userId);
+    Task<Routine> FindById(long id);
+    Task Add(Routine routine);
+    void Update(Routine routine);
+    void Remove(Routine routine);
+}
---
+++ RunningGo.API/Rutinas/Persistence/Repositories/RoutineRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunningGo.API.Rutinas.Domain.Models;
+using RunningGo.API.Rutinas.Domain.Repositories;
+using RunningGo.API.Shared.Persistence.Contexts;
+using RunningGo.API.Shared.Persistence.Repositories;
+
+namespace RunningGo.API.Rutinas.Persistence.Repositories;
+
+public class RoutineRepository: BaseRepository, IRoutineRepository
+{
+    public RoutineRepository(EnhancedDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Routine>> List()
+    {
+        return await _context.Routines.Include(p => p.Habit)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Routine>> ListByUserId(long userId)
+    {
+        return await _context.Routines
+            .Where(p => p.UserId == userId)
+            .Include(p => p.Habit)
+            .ToListAsync();
+    }
+
+    public async Task<Routine> FindById(long id)
+    {
+        return await _context.Routines.FindAsync(id);
+    }
+
+    public async Task Add(Routine routine)
+    {
+        await _context.Routines.AddAsync(routine);
+    }
+
+    public void Update(Routine routine)
+    {
+        _context.Routines.Update(routine);
+    }
+
+    public void Remove(Routine routine)
+    {
+        _context.Routines.Remove(routine);
+    }
+}
