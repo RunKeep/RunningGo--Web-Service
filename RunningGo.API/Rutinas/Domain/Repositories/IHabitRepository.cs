350bca17e0d7b4a811d10c1f889ec6d6a44c2eac
mauriprado <104120014+mauriprado@users.noreply.github.com>
2022-06-13 01:32:18 -0500

    feat(routines): Added and implemented Habit repository.

---
+++ RunningGo.API/Rutinas/Domain/Repositories/IHabitRepository.cs
@@ -0,0 +1,12 @@
+﻿using RunningGo.API.Rutinas.Domain.Models;
+
+namespace RunningGo.API.Rutinas.Domain.Repositories;
+
+public interface IHabitRepository
+{
+    Task<IEnumerable<Habit>> List();
+    Task Add(Habit habit);
+    Task<Habit> FindById(int id);
+    void Update(Habit habit);
+    void Remove(Habit habit);
+}
---
+++ RunningGo.API/Rutinas/Persistence/Repositories/HabitRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunningGo.API.Rutinas.Domain.Models;
+using RunningGo.API.Rutinas.Domain.Repositories;
+using RunningGo.API.Shared.Persistence.Contexts;
+using RunningGo.API.Shared.Persistence.Repositories;
+
+namespace RunningGo.API.Rutinas.Persistence.Repositories;
+
+public class HabitRepository: BaseRepository, IHabitRepository
+{
+    public HabitRepository(EnhancedDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Habit>> List()
+    {
+        return await _context.Habits.ToListAsync();
+    }
+
+    public async Task Add(Habit habit)
+    {
+        await _context.Habits.AddAsync(habit);
+    }
+
+    public async Task<Habit> FindById(int id)
+    {
+        return await _context.Habits.FindAsync(id);
+    }
+
+    public void Update(Habit habit)
+    {
+        _context.Habits.Update(habit);
+    }
+
+    public void Remove(Habit habit)
+    {
+        _context.Habits.Remove(habit);
+    }
+}
