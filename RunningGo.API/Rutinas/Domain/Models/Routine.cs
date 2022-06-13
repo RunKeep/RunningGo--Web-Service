cbb4369295a19f0cdf34da39b00128703076c052
mauriprado <104120014+mauriprado@users.noreply.github.com>
2022-06-13 16:34:22 -0500

    feat(routines): Added Routine model and resources.

---
+++ RunningGo.API/Rutinas/Domain/Models/Routine.cs
@@ -0,0 +1,17 @@
+﻿using RunningGo.API.Shared.Domain.Models;
+
+namespace RunningGo.API.Rutinas.Domain.Models;
+
+public class Routine
+{
+    public long Id { set; get; }
+    public string Name { set; get; }
+    public DateTime Date { set; get; }
+    public string State { set; get; }
+    
+    public long UserId { set; get; }
+    public User User { set; get; }
+    
+    public int HabitId { set; get; }
+    public Habit Habit { set; get; }
+}
---
+++ RunningGo.API/Rutinas/Resources/RoutineResource.cs
@@ -0,0 +1,12 @@
+﻿namespace RunningGo.API.Rutinas.Resources;
+
+public class RoutineResource
+{
+    public long Id { set; get; }
+    public string Name { set; get; }
+    public DateTime Date { set; get; }
+    public string State { set; get; }
+    
+    public long UserId { set; get; }
+    public int HabitId { set; get; }
+}
