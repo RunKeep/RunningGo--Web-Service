aa784b636393ffb7fecedf3c90fd9b2dad2b9318
mauriprado <104120014+mauriprado@users.noreply.github.com>
2022-06-14 15:08:38 -0500

    fix(routines): Fixed routines validation.

--- RunningGo.API/Rutinas/Domain/Repositories/IRoutineRepository.cs
+++ RunningGo.API/Rutinas/Domain/Repositories/IRoutineRepository.cs
@@ -8,6 +8,7 @@ public interface IRoutineRepository
     Task<IEnumerable<Routine>> ListByUserId(long userId);
     Task<Routine> FindById(long id);
     Task<Routine> FindByName(string name);
+    Task<Routine> FindByNameAndUserId(string name, long userId);
     Task Add(Routine routine);
     void Update(Routine routine);
     void Remove(Routine routine);
